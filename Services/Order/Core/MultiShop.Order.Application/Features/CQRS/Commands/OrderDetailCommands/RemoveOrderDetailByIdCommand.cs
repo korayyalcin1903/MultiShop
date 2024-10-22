0c9bf8dd9c1ccdb910af191b4a25ef6dec4eeac9
korayyalcin1903 <korayyalcin1903@gmail.com>
2024-10-22 20:27:58 +0300

    Mediator tasarım

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Commands/OrderDetailCommands/RemoveOrderDetailByIdCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands
+{
+    public class RemoveOrderDetailByIdCommand
+    {
+        public int Id { get; set; }
+
+        public RemoveOrderDetailByIdCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
